dc12fb9aba84e91269a98a4743c9199946ec571d
Megan Horsfield <meganthadev@gmail.com>
2020-08-31 23:52:53 +0000

    Automatically backed up by Learn

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -7,6 +7,7 @@ export default function manageTodo(state = {
   switch (action.type) {
     case 'ADD_TODO':
 
+
       return { todos: state.todos.concat(action.payload.text) };
       
       case 'DELETE_TODO':
